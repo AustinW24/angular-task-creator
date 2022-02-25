0bb876a3f41e19dcd5f73eefe8611969059ebea7
Austin Williams <austinwill264@gmail.com>
2022-02-24 17:07:13 -0800

    routing

--- angular-crash/src/app/components/header/header.component.ts
+++ angular-crash/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UiService} from '../../services/ui.service';
 import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,8 +12,9 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription?: Subscription;
 
-  constructor(private uiService: UiService) {
-      this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  constructor(private uiService: UiService, private router: Router) {
+      this.subscription = this.uiService.onToggle().subscribe(value =>
+        this.showAddTask = value);
   }
 
   ngOnInit(): void {
@@ -21,4 +23,9 @@ export class HeaderComponent implements OnInit {
   toggleAddTask(){
       this.uiService.toggleAddTask();
   }
+
+  hasRoute(route: string){
+      return this.router.url === route;
+  }
+
 }
